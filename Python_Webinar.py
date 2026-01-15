3bdb56ac00e49b6f34ae52409bf61fbe32dbcea6
Ngoc An Nguyen <andyannguyen08@gmail.com>
2026-01-14 17:25:19 -0800

    Python_Webinar.py

--- Python_Webinar.py
+++ Python_Webinar.py
@@ -8,6 +8,14 @@ def generator (n):
     for i in range (n):
         yield 1 + i
 
+#function to add ages
+def age (E, M, D):
+    sum = E + M + D
+    return sum
+
+#Total age
+total = age (16, 16, 17)
+
 it = generator (3)
 
  #for loop to repeatedly print out names
@@ -17,3 +25,4 @@ for i in it:
         print (random [1:3])
         print (pairs)
         
+print ("Their total age is ", total )  
