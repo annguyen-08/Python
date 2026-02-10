c33861476fbd0c011a7f065b11f7416f8e03c60b
Ngoc An Nguyen <andyannguyen08@gmail.com>
2026-02-10 14:44:47 -0800

    Nguyen_Andy_8.1.4.1.2.py

---
+++ Nguyen_Andy_8.1.4.1.2.py
@@ -0,0 +1,17 @@
+svcte = ["m", 'e', 'c', 'h', 'a', 't', 'r', 'o', 'n', 'i', 'c', 's']
+print (svcte[:-5])
+
+svcte[5] = 'y'
+print (svcte)
+
+svcte.append('e')
+print (svcte)
+
+svcte.pop(4)
+print (svcte)
+
+del svcte [3]
+print (svcte)
+
+svcte += ['engineering']
+print (svcte)
